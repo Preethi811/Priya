188fe2d6340d7ec820923e82b18060d9b2151210
Preethi811 <101103530+Preethi811@users.noreply.github.com>
2022-05-17 16:07:36 +0530

    Create sub.java

---
+++ sub.java
@@ -0,0 +1,15 @@
+package devops;
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =6;
+		int b=4;
+		int sub = a -b;
+		System.out.println(sub);
+				
+
+	}
+
+}
